d004b7935606723c18a69dd6c4e6f363b72aa131
Dylan Socolobsky <dsocolobsky@gmail.com>
2022-12-22 15:31:02 -0300

    parser: basic parsing (whole chapter 6)

--- lib/rubylox/parser.rb
+++ lib/rubylox/parser.rb
@@ -1,7 +1,13 @@
 module Rubylox
   class Parser
+
+    def initialize(tokens)
+      @tokens = tokens
+      @current = 0
+    end
+
     def parse(expression)
-      raise NotImplementedError
+      parse_expression
     end
 
     def visit_grouping_expression(expr)
@@ -18,6 +24,158 @@ module Rubylox
 
     def visit_binary_expression(expr)
       raise NotImplementedError
+    end
+
+    def parse_expression
+      parse_equality
+    end
+
+    def parse_equality
+      expr = parse_comparison
+
+      while match(:bang_equal, :equal_equal)
+        operator = previous
+        right = parse_comparison
+        expr = BinaryExpression.new(expr, operator, right)
+      end
+
+      expr
+    end
+
+    def parse_comparison
+      expr = parse_term
+
+      while match(:greater, :greater_equal, :less, :less_equal)
+        operator = previous
+        right = parse_term
+        expr = BinaryExpression.new(expr, operator, right)
+      end
+
+      expr
+    end
+
+    def parse_term
+      expr = parse_factor
+
+      while match(:minus, :plus)
+        operator = previous
+        right = parse_factor
+        expr = BinaryExpression.new(expr, operator, right)
+      end
+
+      expr
+    end
+
+    def parse_factor
+      expr = parse_unary
+
+      while match(:slash, :star)
+        operator = previous
+        right = parse_unary
+        expr = BinaryExpression.new(expr, operator, right)
+      end
+
+      expr
+    end
+
+    def parse_unary
+      if match(:bang, :minus)
+        operator = previous
+        right = parse_unary
+        return UnaryExpression.new(operator, right)
+      end
+
+      parse_primary
+    end
+
+    def parse_primary
+      if match(:false)
+        LiteralExpression.new(false)
+      elsif match(:true)
+        LiteralExpression.new(true)
+      elsif match(:nil)
+        LiteralExpression.new(nil)
+      elsif match(:identifier)
+        LiteralExpression.new(previous.lexeme)
+      elsif match(:number, :string)
+        LiteralExpression.new(previous.literal)
+      elsif match(:left_paren)
+        expr = parse_expression
+        consume(:right_paren, "Expect ')' after expression.")
+        return GroupingExpression.new(expr)
+      else
+        raise error(peek, "Expect expression.")
+      end
+    end
+
+    def match(*types)
+      types.each do |type|
+        if peek_is?(type)
+          advance
+          return true
+        end
+      end
+
+      false
+    end
+
+    def consume(type, message)
+      return advance if peek_is?(type)
+
+      error(peek, message)
+    end
+
+    def error(token, message)
+      if token.type == :eof
+        report(token.line, "end", message)
+      else
+        report(token.line, token.lexeme, message)
+      end
+    end
+
+    def report(line, where, message)
+      "[line #{line}] Error at '#{where}': #{message}"
+    end
+
+    def synchronize
+      advance
+
+      until at_end? || previous.type == :semicolon
+        return if peek.type == :class ||
+          peek.type == :fun ||
+          peek.type == :var ||
+          peek.type == :for ||
+          peek.type == :if ||
+          peek.type == :while ||
+          peek.type == :print ||
+          peek.type == :return
+
+        advance
+      end
+    end
+
+    def peek_is?(type)
+      if at_end?
+        return false
+      end
+      peek.type == type
+    end
+
+    def advance
+      @current += 1
+      previous
+    end
+
+    def at_end?
+      peek.type == :eof
+    end
+
+    def peek
+      @tokens[@current]
+    end
+
+    def previous
+      @tokens[@current - 1]
     end
   end
 end
--- test/test_parser.rb
+++ test/test_parser.rb
@@ -4,7 +4,111 @@ require 'rubylox/expressions'
 require 'rubylox/parser'
 
 class TestParser < Minitest::Test
-  def test_parse
-    assert false
+
+  def test_parse_number
+    tokens = Rubylox::Scanner.new('42').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    res = parser.parse(tokens)
+    assert_instance_of Rubylox::LiteralExpression, res
+    assert_equal 42.0, res.value
+  end
+
+  def test_parse_decimal_number
+    tokens = Rubylox::Scanner.new('3.14').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    res = parser.parse(tokens)
+    assert_instance_of Rubylox::LiteralExpression, res
+    assert_equal 3.14, res.value
+  end
+
+  def test_parse_unary
+    tokens = Rubylox::Scanner.new('!1').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    res = parser.parse(tokens)
+    assert_instance_of Rubylox::UnaryExpression, res
+    assert_equal :bang, res.operator.type
+    assert_equal 1.0, res.right.value
+  end
+
+  def test_parse_double_negation
+    tokens = Rubylox::Scanner.new('!!bar').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    neg_neg_bar = parser.parse(tokens)
+    assert_instance_of Rubylox::UnaryExpression, neg_neg_bar
+    assert_equal :bang, neg_neg_bar.operator.type
+
+    neg_bar = neg_neg_bar.right
+    assert_instance_of Rubylox::UnaryExpression, neg_bar
+    assert_equal :bang, neg_bar.operator.type
+    assert_equal 'bar', neg_bar.right.value
+  end
+
+  def test_parse_term
+    tokens = Rubylox::Scanner.new('1 + 2 + 3').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    sum = parser.parse(tokens)
+    assert_instance_of Rubylox::BinaryExpression, sum
+    assert_equal :plus, sum.operator.type
+    assert_equal 3.0, sum.right.value
+
+    one_plus_two = sum.left
+    assert_instance_of Rubylox::BinaryExpression, one_plus_two
+    assert_equal :plus, one_plus_two.operator.type
+    assert_equal 1.0, one_plus_two.left.value
+    assert_equal 2.0, one_plus_two.right.value
+  end
+
+  def test_parse_factor
+    tokens = Rubylox::Scanner.new('2 * 3 * 9').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    multiplication = parser.parse(tokens)
+    assert_instance_of Rubylox::BinaryExpression, multiplication
+    assert_equal :star, multiplication.operator.type
+    assert_equal 9.0, multiplication.right.value
+
+    two_times_three = multiplication.left
+    assert_instance_of Rubylox::BinaryExpression, two_times_three
+    assert_equal 2.0, two_times_three.left.value
+    assert_equal :star, two_times_three.operator.type
+    assert_equal 3.0,two_times_three.right.value
+  end
+
+  def test_parse_grouping
+    tokens = Rubylox::Scanner.new('(42)').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    res = parser.parse(tokens)
+    assert_instance_of Rubylox::GroupingExpression, res
+    assert_equal 42.0, res.expression.value
+  end
+
+  def test_parse_grouping_complex
+    tokens = Rubylox::Scanner.new('(4 - 3)').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    res = parser.parse(tokens)
+    assert_instance_of Rubylox::GroupingExpression, res
+    inner = res.expression
+    assert_instance_of Rubylox::BinaryExpression, inner
+    assert_equal 4.0, inner.left.value
+    assert_equal :minus, inner.operator.type
+    assert_equal 3.0, inner.right.value
+  end
+
+  def test_parse_arithmetic
+    tokens = Rubylox::Scanner.new('1 + 2 * 3').scan_tokens
+    parser = Rubylox::Parser.new(tokens)
+
+    res = parser.parse(tokens)
+    assert_instance_of Rubylox::BinaryExpression, res
+    assert_equal :plus, res.operator.type
+    assert_equal 1.0, res.left.value
+    assert_equal 6.0, res.right.value
   end
 end
