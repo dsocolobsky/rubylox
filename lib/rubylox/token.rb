0008a37ac07c35bff28b083dce9b541ba455b5d0
Dylan Socolobsky <dsocolobsky@gmail.com>
2023-01-04 20:10:54 -0300

    token: assert valid token type

--- lib/rubylox/token.rb
+++ lib/rubylox/token.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
 module Rubylox
+
+  TOKEN_TYPES = %i[
+    and class else false for fun if nil or print return super this true
+    var while eof error identifier string number left_paren right_paren
+    left_brace right_brace comma dot minus plus semicolon slash star bang
+    bang_equal equal equal_equal greater greater_equal less less_equal
+  ].freeze
+
   class TokenType
     def initialize(type)
+      raise "Unknown token type #{type}" unless TOKEN_TYPES.include?(type)
+
       @type = type
     end
 
--- test/test_token.rb
+++ test/test_token.rb
@@ -5,6 +5,20 @@ require 'rubylox/scanner'
 require 'rubylox/token'
 
 class TestToken  < Minitest::Test
+  def test_create_valid_token_types
+    token_if = Rubylox::TokenType.new(:if)
+    token_while = Rubylox::TokenType.new(:while)
+
+    assert_equal :if, token_if.type
+    assert_equal :while, token_while.type
+  end
+
+  def test_create_invalid_token_type
+    assert_raises RuntimeError do
+      Rubylox::TokenType.new(:invalid)
+    end
+  end
+
   def test_create_a_token
     token = Rubylox::Token.new(:number, '42', 42, 1)
     assert_equal :number, token.type
